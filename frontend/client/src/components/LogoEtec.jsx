b1247c5adb817bac7fad9b5251b68bace9e2c129
Murilo Weishaupt <muriloweishaupt@gmail.com>
2024-11-06 17:20:17 -0300

    Adicionada nova logo e novos professores a base de dados

--- frontend/client/src/components/LogoEtec.jsx
+++ frontend/client/src/components/LogoEtec.jsx
@@ -2,7 +2,7 @@ import './LogoEtec.css'
 
 function LogoEtec() {
     return (
-        <img src="https://yt3.googleusercontent.com/3eQNqXpiinqRSEUvFMkmHAcaeYpt8L5w05u-0kbSJyKcQgtVpeFkcpW6MvGlSL5vazkedz4l=s900-c-k-c0x00ffffff-no-rj" alt="Logo Etec Embu" />
+        <img src="/image/etec-image.png" alt="Logo Etec Embu" />
     )
 }
 
